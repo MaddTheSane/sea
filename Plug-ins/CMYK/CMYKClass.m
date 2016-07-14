f3afb2747b18f68cf9d2c1312f23435d13fc5174
C.W. Betts <computers57@hotmail.com>
2016-07-14 13:37:33 -0600

    Update the CMYK plug-in to the new ColorSync APIs.

--- Plug-ins/CMYK/CMYKClass.m
+++ Plug-ins/CMYK/CMYKClass.m
@@ -28,94 +28,69 @@
 - (void)run
 {
 	PluginData *pluginData = [self.seaPlugins data];
-	IntRect selection;
-	unsigned char *data, *overlay, *replace;
-	int pos, i, j, width, spp, channel;
-	CMBitmap srcBitmap, destBitmap;
-	CMProfileRef srcProf, destProf;
-	CMDeviceID device;
-	CMDeviceProfileID deviceID;
-	CMProfileLocation profileLoc;
-	CMWorldRef cw, scw;
-	
 	[pluginData setOverlayOpacity:255];
 	[pluginData setOverlayBehaviour:kReplacingBehaviour];
-	selection = [pluginData selection];
-	spp = [pluginData spp];
-	width = [pluginData width];
-	data = [pluginData data];
-	overlay = [pluginData overlay];
-	replace = [pluginData replace];
-	channel = [pluginData channel];
+	IntRect selection = [pluginData selection];
+	int width = [pluginData width];
+	unsigned char *data = [pluginData data];
+	unsigned char *overlay = [pluginData overlay];
+	unsigned char *replace = [pluginData replace];
+	int channel = [pluginData channel];
 	
-	// FIXME: What is the replacement API!?
-	// TODO: convert to ColorSyncTransformRef
-	CMGetDefaultDevice(cmDisplayDeviceClass, &device);
-	CMGetDeviceDefaultProfileID(cmDisplayDeviceClass, device, &deviceID);
-	CMGetDeviceProfile(cmDisplayDeviceClass, device, deviceID, &profileLoc);
-	CMOpenProfile(&srcProf, &profileLoc);
-	CMGetDefaultProfileBySpace(cmCMYKData, &destProf);
-	NCWNewColorWorld(&cw, srcProf, destProf);
-	NCWNewColorWorld(&scw, destProf, srcProf);
+	ColorSyncProfileRef srcProf = ColorSyncProfileCreateWithDisplayID(0);
+	ColorSyncProfileRef destProf = ColorSyncProfileCreateWithName(kColorSyncGenericCMYKProfile);
+	NSArray *profSeq = @[
+				@{(__bridge NSString*)kColorSyncProfile: (__bridge id)srcProf,
+				  (__bridge NSString*)kColorSyncRenderingIntent: (__bridge NSString*)kColorSyncRenderingIntentPerceptual,
+				  (__bridge NSString*)kColorSyncTransformTag: (__bridge NSString*)kColorSyncTransformDeviceToPCS,
+				  },
+				
+				@{(__bridge NSString*)kColorSyncProfile: (__bridge id)destProf,
+				  (__bridge NSString*)kColorSyncRenderingIntent: (__bridge NSString*)kColorSyncRenderingIntentPerceptual,
+				  (__bridge NSString*)kColorSyncTransformTag: (__bridge NSString*)kColorSyncTransformPCSToPCS,
+				  },
+
+				@{(__bridge NSString*)kColorSyncProfile: (__bridge id)srcProf,
+				  (__bridge NSString*)kColorSyncRenderingIntent: (__bridge NSString*)kColorSyncRenderingIntentPerceptual,
+				  (__bridge NSString*)kColorSyncTransformTag: (__bridge NSString*)kColorSyncTransformPCSToDevice,
+				  },
+				];
 	
-	for (j = selection.origin.y; j < selection.origin.y + selection.size.height; j++) {
-		pos = j * width + selection.origin.x;
+	ColorSyncTransformRef cw = ColorSyncTransformCreate((__bridge CFArrayRef)(profSeq), NULL);
+	
+	for (int j = selection.origin.y; j < selection.origin.y + selection.size.height; j++) {
+		int pos = j * width + selection.origin.x;
+		
+		ColorSyncDataLayout srcLayout = kColorSyncByteOrderDefault;
+		ColorSyncDataDepth srcDepth = kColorSync8BitInteger;
+		size_t srcRowBytes;
+		void *srcBytes;
+		void *dstBytes = &(overlay[pos * 4]);
 		
 		if (channel == kPrimaryChannels) {
-			srcBitmap.image = (char *)&(data[pos * 3]);
-			srcBitmap.width = selection.size.width;
-			srcBitmap.height = 1;
-			srcBitmap.rowBytes = selection.size.width * 3;
-			srcBitmap.pixelSize = 8 * 3;
-			srcBitmap.space = cmRGB24Space;
+			srcBytes = &(data[pos * 3]);
+			srcRowBytes = selection.size.width * 3;
+			srcLayout |= kColorSyncAlphaNone;
 		} else {
-			srcBitmap.image = (char *)&(data[pos * 4]);
-			srcBitmap.width = selection.size.width;
-			srcBitmap.height = 1;
-			srcBitmap.rowBytes = selection.size.width * 4;
-			srcBitmap.pixelSize = 8 * 4;
-			srcBitmap.space = cmRGBA32Space;
+			srcBytes = &(data[pos * 4]);
+			srcRowBytes = selection.size.width * 4;
+			srcLayout |= kColorSyncAlphaLast;
 		}
 		
-		destBitmap.image = (char *)&(overlay[pos * 4]);
-		destBitmap.width = selection.size.width;
-		destBitmap.height = 1;
-		destBitmap.rowBytes = selection.size.width * 4;
-		destBitmap.pixelSize = 8 * 4;
-		destBitmap.space = cmCMYK32Space;
+		ColorSyncTransformConvert(cw, selection.size.width, 1, dstBytes, kColorSync8BitInteger, kColorSyncAlphaLast | kColorSyncByteOrderDefault, selection.size.width * 4, srcBytes, srcDepth, srcLayout, srcRowBytes, NULL);
 		
-		// FIXME: What is the replacement API!?
-		CWMatchBitmap(cw, &srcBitmap, NULL, 0, &destBitmap);
-		
-		srcBitmap.image = (char *)&(overlay[pos * 4]);
-		srcBitmap.width = selection.size.width;
-		srcBitmap.height = 1;
-		srcBitmap.rowBytes = selection.size.width * 4;
-		srcBitmap.pixelSize = 8 * 4;
-		srcBitmap.space = cmCMYK32Space;
-		
-		destBitmap.image = (char *)&(overlay[pos * 4]);
-		destBitmap.width = selection.size.width;
-		srcBitmap.height = 1;
-		destBitmap.rowBytes = selection.size.width * 4;
-		destBitmap.pixelSize = 8 * 4;
-		destBitmap.space = cmRGBA32Space;
-		
-		CWMatchBitmap(scw, &srcBitmap, NULL, 0, &destBitmap);
-		
-		for (i = selection.size.width; i >= 0; i--) {
+		for (int i = selection.size.width; i >= 0; i--) {
 			if (channel == kPrimaryChannels)
 				overlay[(pos + i) * 4 + 3] = 255;
 			else
 				overlay[(pos + i) * 4 + 3] = data[(pos + i) * 4 + 3];
 			replace[pos + i] = 255;
 		}
-		
 	}
 	
-	CWDisposeColorWorld(cw);
-	CWDisposeColorWorld(scw);
-	CMCloseProfile(srcProf);
+	CFRelease(cw);
+	CFRelease(srcProf);
+	CFRelease(destProf);
 	
 	[pluginData apply];
 }
