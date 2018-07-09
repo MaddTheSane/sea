a8b9318e1b4dd8476f6128240df2006c7b3f4394
C.W. Betts <computers57@hotmail.com>
2018-07-09 10:30:30 -0600

    Add nullability metadata to SeaContent

--- Seashore/source/tool/EraserTool.swift
+++ Seashore/source/tool/EraserTool.swift
@@ -177,7 +177,7 @@ class EraserTool: AbstractTool, SeaOptions {
 		}
 
 		// Determine background colour and hence the brush colour
-		let color = document.contents.background!
+		let color = document.contents.background
 		if spp == 4 {
 			basePixel[0] = UInt8(color.redComponent * 255)
 			basePixel[1] = UInt8(color.greenComponent * 255)
