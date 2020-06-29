c1731640b8307bd2e9d5f7338a33d2d954fcab55
C.W. Betts <computers57@hotmail.com>
2020-06-28 20:42:54 -0600

    Minor pokes to ImageToolbarItem.

--- Seashore/source/application/ImageToolbarItem.m
+++ Seashore/source/application/ImageToolbarItem.m
@@ -4,19 +4,21 @@
 
 -(ImageToolbarItem *)initWithItemIdentifier:  (NSString*) itemIdent label:(NSString *) label image:(NSImage *) image toolTip: (NSString *) toolTip target: (id) target selector: (SEL) selector
 {
-	self = [super initWithItemIdentifier: itemIdent];
+	if (self = [super initWithItemIdentifier: itemIdent]) {
 	
 	// Set the text label to be displayed in the toolbar and customization palette
 	[self setLabel: label];
 	[self setPaletteLabel: label];
 	
-	// Set up a reasonable tooltip, and image   Note, these aren't localized, but you will likely want to localize many of the item's properties
+	// Set up a reasonable tooltip, and image
+	// Note, these aren't localized, but you will likely want to localize many of the item's properties
 	[self setToolTip: toolTip];
 	[self setImage: image];
 	
 	// Tell the item what message to send when it is clicked
 	[self setTarget: target];
 	[self setAction: selector];
+	}
 	
 	return self;
 }
